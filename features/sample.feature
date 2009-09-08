9cc327024a6679f4b6f61d30d418a2bfc828b4fe
Rich Downie <rich@richdownie.com>
2009-09-07 20:53:04 -0400

    buttons is tight now

--- features/sample.feature
+++ features/sample.feature
@@ -1,13 +1,21 @@
 Feature: Testing watircuke at richdownie.com
 
+  Scenario: testing buttons by id
+  Given I am on the passenger page
+  And I click the "cuke" button
+
   Scenario: testing buttons by value
     Given I am on the passenger page
     And I click the "Rubyist" button
 
-  Scenario: testing buttons by id
-    Given I am on the passenger page
-    And I click the "cuke" button
+    Scenario: testing buttons by name
+      Given I am on the passenger page
+      And I click the "bar" button
 
   Scenario: testing buttons by class
     Given I am on the passenger page
-    And I click the "watir" button              
+    And I click the "watir" button
+
+  Scenario: testing buttons by index
+    Given I am on the passenger page
+    And I click the "1" button          
--- features/step_definitions/buttons.rb
+++ features/step_definitions/buttons.rb
@@ -1,4 +1,4 @@
-# Try running with the button ":id", ":name", ":value", ":text", ":index" or ":class" element attribute.
+# Try running with the button ":id", ":name", ":value", ":index" or ":class" element attribute.
 # Does not matter what you select!
 # The proper watir code will be executed regardless.
 
@@ -12,16 +12,13 @@ Given /I click the "(.*)" button/ do |type|
   elsif @browser.button(:value, type).exists? then
         @browser.button(:value, type).click
   
-  elsif @browser.button(:text, type).exists? then
-        @browser.button(:text, type).click
-  
   elsif @browser.button(:index, type).exists? then
         @browser.button(:index, type).click
  
   elsif @browser.button(:class, /(^|\s)#{type}(\s|$)/).exists? then
        @browser.button(:class, /(^|\s)#{type}(\s|$)/).click
   else
-    fail("could not find what you asked for")
+    fail("could not find #{type}")
   end
 end
 
