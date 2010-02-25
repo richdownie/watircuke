b11161104ed107276c834b7db50c24fcfed2f33b
Rich Downie <rich@richdownie.com>
2010-02-25 13:22:38 -0500

    sleep step

--- features/step_definitions/watircuke_steps.rb
+++ features/step_definitions/watircuke_steps.rb
@@ -47,6 +47,10 @@ Given /^I am on (.+)$/ do |page_name|
   @browser.goto(path_to(page_name))  #This step links up with the "path_to" method found in support/paths.rb
 end
 
-Given /I go to "(.*)"/ do |url|
+Given /^I go to "([^\"]*)"$/ do |url|
   @browser.goto(url)                 #Links to generic urls like "google.com"
+end
+
+Given /^I sleep for "([^\"]*)"$/ do |seconds|
+  sleep seconds.to_i
 end
