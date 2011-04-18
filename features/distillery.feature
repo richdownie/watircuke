757a07311bbb7791653a4099637317187882c82f
Rich Downie <rich@richdownie.com>
2011-04-18 13:32:29 -0400

    fixed

--- features/distillery.feature
+++
@@ -1,6 +0,0 @@
-Feature: testing the distillery website
-
-Scenario: Confirm Locations
-  * I am on the distillery page
-  * I click the "Contact Us" link
-  * I should see the text "Mt. Hope"
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -20,9 +20,6 @@ module Paths
       when /the youtube page/i
         @environment + "youtube.com"
         
-      when /the distillery page/i
-        @environment + "thedistillery.com"
-        
     else
       raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
         "Now, go and add a mapping in #{__FILE__}"
